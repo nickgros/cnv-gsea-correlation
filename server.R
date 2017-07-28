60b27a9c06402b30718cfb8c6517f459306d99df
Nick Grosenbacher <grosenbacher.1@osu.edu>
2017-07-28 11:01:32 -0600

    refactored server.ui to be relatively cooperative

--- server.R
+++ server.R
@@ -7,51 +7,53 @@ options(shiny.maxRequestSize=500*1024^2)
 #correl.table <- NULL
 server <- function(input, output, session) {
   
-  # input$file1 will be NULL initially. After the user selects
-  # and uploads a file, it will be a data frame with 'name',
-  # 'size', 'type', and 'datapath' columns. The 'datapath'
-  # column will contain the local filenames where the data can
-  # be found.
-  output$table <- renderDataTable({
+  tables <- reactiveValues()
+  
+  
+  output$band <- renderUI({
+    req(input$cnvFile)
+    tables$cnv.table <- fread(input$cnvFile$datapath, data.table = F)
+    selectInput(inputId = "band", label = "Select a band(s) to plot.",
+                choices = unique(tables$cnv.table$band),  multiple = T)
+  })
+  
+  
+  output$signature <- renderUI({
+    req(input$gseaFile)
+    tables$gsea.table <- fread(input$gseaFile$datapath, data.table = F)
+    selectInput(inputId = "signature", label = "Select a signature to plot.",
+                choices = unique(tables$gsea.table[,1]),  multiple = F)
+  })
+
+  output$category <- renderUI({
+    req(input$categoryFile)
     
+    tables$category.table <- fread(input$categoryFile$datapath, data.table = FALSE)
+    checkboxGroupInput(inputId = "category",
+                       label = colnames(tables$category.table)[2],
+                       choices = unique(tables$category.table[,2]))
+    
+  })
+  
+  # Table that lists all correlations with bands and relevant statistics.
+  # Requires user to upload the table generated in the computation script.
+  output$table <- renderDataTable({
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, it will be a data frame with 'name',
     # 'size', 'type', and 'datapath' columns. The 'datapath'
     # column will contain the local filenames where the data can
     # be found.
     inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
+    req(inFile)
     correl.table <<- fread(inFile$datapath, data.table = F)
-    updateSelectInput(session,inputId = "signature",
-                      choices = unique(correl.table$signature))
     return(correl.table)
   })
   
   
-  
-  output$band = renderUI({
-    selectInput('band', 'Select one or more bands to plot:', 
-                multiple = T,
-                choices = correl.table$band[which(correl.table$signature==input$signature)])
-  })
+
   
 
-  # Reactive expression to generate the requested distribution. This is 
-  # called whenever the inputs change. The renderers defined 
-  # below then all use the value computed from this expression
-  data <- reactive({  
-    dist <- switch(input$dist,
-                   norm = rnorm,
-                   unif = runif,
-                   lnorm = rlnorm,
-                   exp = rexp,
-                   rnorm)
-    
-    dist(input$n)
-  })
-  
+
   # Generate a plot of the data. Also uses the inputs to build the 
   # plot label. Note that the dependencies on both the inputs and
   # the 'data' reactive expression are both tracked, and all expressions 
@@ -59,20 +61,20 @@ server <- function(input, output, session) {
   output$plot <- renderPlot({
     cnvFile <- input$file2
     gseaFile <- input$file3
-    #sepFile <- input$file4
-    if (is.null(cnvFile) || is.null(gseaFile))
-      return(NULL)
     
-    cnv.table <<- fread(cnvFile$datapath, data.table = F)
-    gsea.table <<- fread(gseaFile$datapath, data.table = F)
-
-    if (is.null(input$band))
-      return(NULL)
+    req(tables$cnv.table, tables$gsea.table, input$band)
     
-    plot.df <- cnv.table %>% filter(band %in% input$band)
+    
+    plot.df <- tables$cnv.table %>% filter(band %in% input$band)
     plot.df$band <- factor(plot.df$band)
-    plot.df$phenotype <- as.numeric(gsea.table[which(gsea.table[,1] == input$signature), plot.df$sample])
-    ggplot(plot.df,
+    plot.df$phenotype <- as.numeric(tables$gsea.table[which(tables$gsea.table[,1] == input$signature), plot.df$sample])
+    
+    if(!is.null(tables$category.table) && !is.null(input$category)) {
+      plot.df$category <- tables$category.table[,2][match(x = plot.df$sample, table = tables$category.table$sample)]
+      plot.df <- plot.df[which(plot.df$category %in% input$category),]
+    }
+    
+    plot <- ggplot(plot.df,
                    aes(x = cnv,
                        y = phenotype
                    )) +
@@ -88,7 +90,13 @@ server <- function(input, output, session) {
                       label.x.npc = 'right',
                       label.y.npc = 'bottom',
                       size = 4) +
-      ylab(input$signature) + theme(legend.position = "bottom") + facet_grid( ~ band)
+      ylab(input$signature) + theme(legend.position = "bottom")
+    if (is.null(input$category)) {
+      plot <- plot + facet_grid( ~ band)
+    } else {
+      plot <- plot + facet_grid(category ~ band)
+    }
+    plot
   })
   
   # Generate a summary of the data
